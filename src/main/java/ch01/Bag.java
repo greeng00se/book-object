11728d5a8eb1503f45acb989753523866e0f7843
greeng00se <thegreengoose7@gmail.com>
2022-08-11 02:46:35 +0900

    [#1] feat: 가방 클래스 추가
    - 관객의 소지품이 들어있다.
    - 소지품의 종류는 초대장, 티켓, 현금 세가지다.

---
+++ src/main/java/ch01/Bag.java
@@ -0,0 +1,30 @@
+package ch01;
+
+/**
+ * 관객의 소지품을 보관할 가방
+ */
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
