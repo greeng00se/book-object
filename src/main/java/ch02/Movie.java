0e5f8adecf3470060b0fa60f8af862216dc40802
greeng00se <thegreengoose7@gmail.com>
2022-08-19 16:20:47 +0900

    [#3] feat: 영화 클래스 추가

---
+++ src/main/java/ch02/Movie.java
@@ -0,0 +1,30 @@
+package ch02;
+
+import ch02.policy.DiscountPolicy;
+
+import java.time.Duration;
+
+/**
+ * 영화 클래스
+ */
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+}
