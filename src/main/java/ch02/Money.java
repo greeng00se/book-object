0f2af28b92605f3dca4da410ad96f338d14e48d2
greeng00se <thegreengoose7@gmail.com>
2022-08-19 17:40:45 +0900

    [#3] test: Movie 테스트 추가

--- src/main/java/ch02/Money.java
+++ src/main/java/ch02/Money.java
@@ -1,6 +1,7 @@
 package ch02;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  * 금액
@@ -41,5 +42,22 @@ public class Money {
 
     public boolean isGreaterThanOrEqual(Money other) {
         return amount.compareTo(other.amount) >= 0;
+    }
+
+    public String getAmount() {
+        return amount.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return Objects.equals(amount, money.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
