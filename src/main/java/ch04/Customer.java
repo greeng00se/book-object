7f67522fc62a5e159d7dddb8a5030c2ef96bba07
greeng00se <thegreengoose7@gmail.com>
2022-08-28 21:02:23 +0900

    [#5] feat: 관람객 클래스 추가

---
+++ src/main/java/ch04/Customer.java
@@ -0,0 +1,15 @@
+package ch04;
+
+import lombok.Builder;
+
+public class Customer {
+
+    private String name;
+    private String id;
+
+    @Builder
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
