1da3be796e955d8210088b9c22cdff455d99aa05
greeng00se <thegreengoose7@gmail.com>
2022-08-28 21:32:22 +0900

    [#5] refactor: 상영 클래스에 요금 계산 메서드 추가

--- src/main/java/ch04/Screening.java
+++ src/main/java/ch04/Screening.java
@@ -1,5 +1,7 @@
 package ch04;
 
+import ch02.Money;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,4 +13,30 @@ public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    @Builder
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
 }
