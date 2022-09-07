bf87c5258e4fd17365c14b386a839c7f17471ad5
greeng00se <thegreengoose7@gmail.com>
2022-09-08 00:32:31 +0900

    [ch05] refactor: 분해한 코드들을 적절한 클래스로 이동

--- src/main/java/ch05_02/DiscountCondition.java
+++ src/main/java/ch05_02/DiscountCondition.java
@@ -16,4 +16,22 @@ public class DiscountCondition {
     private DayOfWeek dayOfWeek;
     private LocalTime startTime;
     private LocalTime endTime;
+
+    public boolean isDiscountable(DiscountCondition condition, Screening screening) {
+        if (condition.getType() == DiscountConditionType.PERIOD) {
+            return isSatisfiedByPeriod(condition, screening);
+        }
+
+        return isSatisfiedBySequence(condition, screening);
+    }
+
+    private boolean isSatisfiedByPeriod(DiscountCondition condition, Screening screening) {
+        return screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
+                condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
+                condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
+    }
+
+    private boolean isSatisfiedBySequence(DiscountCondition condition, Screening screening) {
+        return condition.getSequence() == screening.getSequence();
+    }
 }
