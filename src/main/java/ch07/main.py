7d53020e86161b1e246117c36306b0191533998e
greeng00se <thegreengoose7@gmail.com>
2022-09-23 16:24:13 +0900

    [ch07] feat: 아르바이트 직원에 대한 급여에 대한 급여 시스템 기능 추가
    - 변경에 의한 영향이 심각..
    - 만약 거대한 시스템이었다면...?

--- src/main/java/ch07/main.py
+++ src/main/java/ch07/main.py
@@ -1,7 +1,9 @@
 import sys
 
-employees = ["직원A", "직원B", "직원C"]
-basePays = [400, 300, 250]
+employees = ["직원A", "직원B", "직원C", "알바D", "알바E", "알바F"]
+basePays = [400, 300, 250, 1, 1, 1.5]
+hourlys = [False, False, False, True, True, True]
+timeCards = [0, 0, 0, 120, 120, 120]
 
 def getTaxRate():
     return float(input('세율을 입력하세요: '))
@@ -11,21 +13,35 @@ def calculatePayFor(name, taxRate):
     basePay = basePays[index]
     return basePay - (basePay * taxRate)
 
+def calculateHourlyPayFor(name, taxRate):
+    index = employees.index(name)
+    basePay = basePays[index] * timeCards[index]
+    return basePay - (basePay * taxRate)
+
+def isHourly(name):
+    return hourlys[employees.index(name)]
+
 def describeResult(name, pay):
     return '이름: {0}, 급여: {1}'.format(name, pay)
 
 def sumOfBasePays():
     result = 0
-    for basePay in basePays:
-        result += basePay
+    for index, basePay in enumerate(basePays):
+        result += basePay if not hourlys[index] else 0
     print(result)
 
 def calculatePay(name):
     if name not in employees:
         print('올바른 이름을 입력하세요.')
         return
+
     taxRate = getTaxRate()
-    pay = calculatePayFor(name, taxRate)
+    pay = 0
+    if isHourly(name):
+        pay = calculateHourlyPayFor(name, taxRate)
+    else:
+        pay = calculatePayFor(name, taxRate)
+
     print(describeResult(name, pay))
 
 def main(argv):
