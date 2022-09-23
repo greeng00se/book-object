1735ed10e7e1697675e29d29b125d1fb54a1a557
greeng00se <thegreengoose7@gmail.com>
2022-09-23 16:12:10 +0900

    [ch07] feat: 급여 기본급의 총합을 구하는 기능을 추가
    - 기존의 main 함수를 sumOfBasePays로 옮김
    - 결합도가 문제가 된다. 상위 함수가 강요하는 문맥에 강하게 결합된다.

--- src/main/java/ch07/main.py
+++ src/main/java/ch07/main.py
@@ -14,10 +14,25 @@ def calculatePayFor(name, taxRate):
 def describeResult(name, pay):
     return '이름: {0}, 급여: {1}'.format(name, pay)
 
-def main(name):
+def sumOfBasePays():
+    result = 0
+    for basePay in basePays:
+        result += basePay
+    print(result)
+
+def calculatePay(name):
+    if name not in employees:
+        print('올바른 이름을 입력하세요.')
+        return
     taxRate = getTaxRate()
     pay = calculatePayFor(name, taxRate)
     print(describeResult(name, pay))
 
+def main(argv):
+    if argv:
+        calculatePay(argv[0])
+    else:
+        sumOfBasePays()
+
 if __name__ == "__main__":
-	main(sys.argv[1])
+    main(sys.argv[1:])
