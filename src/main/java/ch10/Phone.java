e854778a4629886016bf59232c9bb6757f2a57c7
greeng00se <thegreengoose7@gmail.com>
2022-10-13 23:59:35 +0900

    [ch10] refactor: AbstractPhone을 상속한 Phone 클래스

--- src/main/java/ch10/Phone.java
+++ src/main/java/ch10/Phone.java
@@ -4,33 +4,21 @@ import ch02.Money;
 import lombok.Getter;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
-public class Phone {
+public class Phone extends AbstractPhone {
 
     private Money amount;
     private Duration seconds;
-    private List<Call> calls = new ArrayList<>();
 
     public Phone(Money amount, Duration seconds) {
         this.amount = amount;
         this.seconds = seconds;
     }
 
-    public void call(Call call) {
-        calls.add(call);
-    }
-
-    public Money calculateFee() {
-        Money result = Money.ZERO;
-
-        for (Call call : calls) {
-            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-        }
-
-        return result;
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
     }
 }
 
