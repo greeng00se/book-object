1081007786efa8f7cced506e3c265be9e01940f0
greeng00se <thegreengoose7@gmail.com>
2022-11-01 22:35:28 +0900

    [ch14] feat: 고정요금 방식을 표현하는 클래스명 변경

--- src/main/java/ch14/FixedFeePolicy.java
+++ src/main/java/ch14/FixedFeePolicy.java
@@ -4,11 +4,11 @@ import ch02.Money;
 
 import java.time.Duration;
 
-public class RegularPolicy extends BasicRatePolicy {
+public class FixedFeePolicy extends BasicRatePolicy {
     private Money amount;
     private Duration seconds;
 
-    public RegularPolicy(Money amount, Duration seconds) {
+    public FixedFeePolicy(Money amount, Duration seconds) {
         this.amount = amount;
         this.seconds = seconds;
     }
