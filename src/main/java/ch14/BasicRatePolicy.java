f0dbefbfe95695c907f24e5b37c066f1825d58ea
greeng00se <thegreengoose7@gmail.com>
2022-11-11 21:29:51 +0900

    [ch14] feat: 변하지 않는 부분 - BasicRatePolicy

--- src/main/java/ch14/BasicRatePolicy.java
+++ src/main/java/ch14/BasicRatePolicy.java
@@ -2,18 +2,30 @@ package ch14;
 
 import ch02.Money;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class BasicRatePolicy implements RatePolicy {
+
+    private List<FeeRule> feeRules = new ArrayList<>();
+
+    public BasicRatePolicy(FeeRule ... feeRules) {
+        this.feeRules = Arrays.asList(feeRules);
+    }
 
     @Override
     public Money calculateFee(Phone phone) {
-        Money result = Money.ZERO;
-
-        for (Call call : phone.getCalls()) {
-            result = result.plus(calculateCallFee(call));
-        }
-
-        return result;
+        return phone.getCalls()
+                .stream()
+                .map(call -> calculate(call))
+                .reduce(Money.ZERO, Money::plus);
     }
 
-    protected abstract Money calculateCallFee(Call call);
+    private Money calculate(Call call) {
+        return feeRules
+                .stream()
+                .map(rule -> rule.calculateFee(call))
+                .reduce(Money.ZERO, Money::plus);
+    }
 }
