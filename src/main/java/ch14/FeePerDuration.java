c6ab2184bb9dc1327827405a956b2c6f42d91027
greeng00se <thegreengoose7@gmail.com>
2022-11-11 21:28:32 +0900

    [ch14] feat: 변하지 않는 부분 - 단위 요금 FeePerDuration

---
+++ src/main/java/ch14/FeePerDuration.java
@@ -0,0 +1,22 @@
+package ch14;
+
+import ch02.Money;
+
+import java.time.Duration;
+
+/**
+ * 단위 요금
+ */
+public class FeePerDuration {
+    private Money fee;
+    private Duration duration;
+
+    public FeePerDuration(Money fee, Duration duration) {
+        this.fee = fee;
+        this.duration = duration;
+    }
+
+    public Money calculate(DateTimeInterval interval) {
+        return fee.times(interval.duration().getSeconds() / duration.getSeconds());
+    }
+}
